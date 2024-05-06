5e7bd182860811cadee9c4765731355501dcf085
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-05-06 18:30:12 -0400

    feat: add exemple of services, autowiring and debug:autowiring

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,30 @@
 
 namespace App\Controller;
 
+use App\Service\StringManipulationService;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
-    {
+    public function index(
+        LoggerInterface $logger,
+        HttpClientInterface $httpClient,
+        StringManipulationService $stringManipulationService,
+        Environment $environment,
+    ): Response {
+
+        $test = 'loren[ipson]test]';
+        $newString = $stringManipulationService->cleanString($test);
+
+        $response = $httpClient->request('GET', 'https://127.0.0.1:8000/api/news/22');
+
+        $logger->info('Accessed home page');
 
         $categories = [
             ['title' => 'World',        'text' => 'News about World'],
@@ -26,12 +41,18 @@ class HomeController extends AbstractController
             ['title' => 'Style',        'text' => 'News about Life Style'],
             ['title' => 'Travel',       'text' => 'News about Travels'],
         ];
+        $logger->error('Created Array');
 
         $pageTitle = "News Systems";
-        return $this->render('home/index.html.twig', [
+
+        $logger->warning('Defined Title');
+
+        $html = $environment->render('home/index.html.twig', [
             'categories' => $categories,
             'pageTitle' => $pageTitle,
         ]);
+
+        return new Response($html);
     }
 
     #[Route('/category/{slug}', name: 'app_category', methods: ['GET', 'POST'])]
@@ -53,6 +74,7 @@ class HomeController extends AbstractController
         ];
 
         $pageTitle = "News About " . $slug;
+
         return $this->render('category/index.html.twig', [
             'categories' => $categories,
             'pageTitle' => $pageTitle,
--- src/Service/StringManipulationService.php
+++ src/Service/StringManipulationService.php
@@ -2,7 +2,22 @@
 
 namespace App\Service;
 
-class stringManipulationService
-{
+use Psr\Log\LoggerInterface;
 
+class StringManipulationService
+{
+    private LoggerInterface $logger;
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function cleanString(string $text): string
+    {
+        $this->logger->info('amount received ' . $text);
+        $text = str_replace(array("[", "]"), " ", $text);
+        $this->logger->info('amount returned ' . $text);
+        return $text;
+
+    }
 }
